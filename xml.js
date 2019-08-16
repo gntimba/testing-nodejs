58922970c4bf2866f25fdc22e3601f9902c32e97
godfrey Timba <gntimba@gmail.com>
2019-08-16 16:07:03 +0200

    cb

--- xml.js
+++ xml.js
@@ -28,10 +28,43 @@ var parseString = require('xml2js').parseString;
 
     axiosTest().then(data => {
         parseString(data, function (err, result) {
-            let products= result.rss.channel[0].item[0]['g:description'][0].replace(/<\/?[^>]+(>|$)/g, "")
-           // let products= result.rss.channel[0].item
-            console.log(products.length)
-            res.json(products)
+            //let products= result.rss.channel[0].item[0]['g:description'][0].replace(/<\/?[^>]+(>|$)/g, "")
+            let products= result.rss.channel[0].item
+            // console.log(products.length)
+             //res.json(products)
+            let entries = { entries: [] }
+            
+            for (let index = 0; index < products.length; index++) {
+              let price = products[index]['g:price'][0].split(' ')
+              const params = {
+                merchantId: 134645521,
+                batchId: index + 1,
+                method: "insert",
+                product: {
+                  channel: "online",
+                  contentLanguage: "en",
+                  offerId: products[index]['g:id'][0],
+                  targetCountry: "ZA",
+                  brand: products[index]['g:brand'][0],
+                  title: products[index]['g:title'][0].replace(/<\/?[^>]+(>|$)/g, ""),
+                  link: products[index]['g:link'][0],
+                  description: products[index]['g:description'][0].replace(/<\/?[^>]+(>|$)/g, ""),
+                  imageLink: products[index]['g:image_link'][0],
+                  productTypes:[ products[index]['g:google_product_category'][0]],
+                  price: {
+                    currency: price[1],
+                    value: price[0]
+                  },
+                  availability: products[index]['g:availability'][0],
+                  condition:products[index]['g:condition'][0]
+                
+                }
+              };
+      
+              entries.entries.push(params)
+            
+            }
+            res.json(entries)
             
         })
     })
